3806b73b869dbcfefb5613120b2197c87f0f9f55
RodrigoRea <31930971+RodrigoRea@users.noreply.github.com>
2022-01-20 11:07:07 -0300

    icon splash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,33 @@
 export const environment = {
   production: true
 };
+
+/************************************************************************************ */
+/************************************************************************************ */
+/************************************************************************************ */
+/*
+-- Construir imagens a partir de resources/icon.png
+-- https://ionicframework.com/docs/cli/commands/cordova-resources
+ionic cordova resources
+ionic cordova resources ios
+ionic cordova resources android
+
+-- com problemas - executar remover e instalar
+npm remove cordova-res
+npm install cordova-res
+*/
+/************************************************************************************ */
+/************************************************************************************ */
+/************************************************************************************ */
+// ## IONIC 
+// ## Build Capacitor
+// ionic cap add android
+
+// ionic cap copy
+// ionic cap sync
+
+// ## Abrir no android studio
+// ionic cap open android
+/************************************************************************************ */
+/************************************************************************************ */
+/************************************************************************************ */
