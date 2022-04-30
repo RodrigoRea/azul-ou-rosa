d35c5e94112ac1b2c544649da6971b47aebcf1aa
RodrigoRea <31930971+RodrigoRea@users.noreply.github.com>
2022-04-30 10:41:40 -0300

    Som Útero Materno

--- src/app/pages/audio/audio.page.ts
+++ src/app/pages/audio/audio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdMobGoogleService } from 'src/app/_services/admob-google.service';
 
 @Component({
   selector: 'app-audio',
@@ -9,21 +10,24 @@ export class AudioPage implements OnInit {
 
   status: 'play'|'pause' = 'pause';
 
-  constructor() { }
+  constructor(
+    private adMobGoogleService: AdMobGoogleService
+  ) { }
 
   ngOnInit() {
   }
 
-   
+  ionViewWillEnter(){ this.adMobGoogleService.bannerShow(); }
+  ionViewWillLeave(){ this.adMobGoogleService.bannerHide(); this.pause(); }
 
   play() {
-    var x: any = document.getElementById("myAudio"); 
+    var x: any = document.getElementById("tag-audio"); 
     x.play(); 
     this.status = 'play';
   } 
 
   pause() { 
-    var x: any = document.getElementById("myAudio");
+    var x: any = document.getElementById("tag-audio");
     x.pause(); 
     this.status = 'pause';
   } 
